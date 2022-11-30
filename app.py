68db67c129dc6844d91ac33294f7317123f920e1
Matthew Fernandez <matthew9erfernandez@gmail.com>
2022-11-30 14:23:01 -0800

    Update virtual env & Fns in app.py

--- app.py
+++ app.py
@@ -6,6 +6,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def predict_bond_FV(investings):
+    """
+    Show average annual Total Returns for 1, 3, 5, & 10 years in Bond Market
+    """
+    avg_annual_tot_returns_bond = []
+    one_yr = investings + (investings * (-0.1035))
+    three_yr = investings + (investings * (-0.0093))
+    five_yr = investings + (investings * (0.0086))
+    ten_yr = investings + (investings * (0.0150))
+    avg_annual_tot_returns_bond.append([one_yr, three_yr, five_yr, ten_yr])
+    return avg_annual_tot_returns_bond
+
+def predict_stock_FV(investings):
+    """
+    Show average annual Total Returns for 1, 3, 5, & 10 years in Fidelity 500 Index
+    """
+    avg_annual_tot_returns_stock = []
+    one_yr = investings + (investings * (-0.1063))
+    three_yr = investings + (investings * (0.1059))
+    five_yr = investings + (investings * (0.1129))
+    ten_yr = investings + (investings * (0.1295))
+    avg_annual_tot_returns_stock.append([one_yr, three_yr, five_yr, ten_yr])
+    return avg_annual_tot_returns_stock
+
 @app.route("/")
 def index():
     
